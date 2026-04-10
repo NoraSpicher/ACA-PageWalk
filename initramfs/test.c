fcc414035e60cda02eeb61993e09c166156d1400
NoraSpicher <noraspicher@gmail.com>
2026-04-09 20:49:02 -0600

    Added microbenchmark binaries

--- initramfs/test.c
+++ initramfs/test.c
@@ -6,10 +6,10 @@
 int main()
 {
     void *addr = (void *)0x700000000000ULL;
-    size_t size = 4096 * 4;
+    size_t size = 4096;
 
-    printf("Requesting mmap at %p\n", addr);
-
+    //printf("Requesting mmap at %p\n", addr);
+    
     void *p = mmap(addr, size,
                    PROT_READ | PROT_WRITE,
                    MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED_NOREPLACE,
@@ -20,13 +20,12 @@ int main()
         return 1;
     }
 
-    printf("mmap succeeded, touching pages...\n");
-
+    //printf("mmap succeeded, touching pages...\n");
+    
+    while(1){
     char *c = (char *)p;
     c[0] = 1;
-    c[4096] = 2;
-    c[8192] = 3;
-
-    printf("done\n");
+    }
+//    printf("done\n");
     return 0;
 }
